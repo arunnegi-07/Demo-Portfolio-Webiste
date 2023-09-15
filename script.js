f72b2d65d8ed80fb225f522385d07990ce76d215
arunnegi-07 <reacharunnegi@gmail.com>
2023-09-15 22:28:47 +0530

    Work in Progress

---
+++ script.js
@@ -0,0 +1,49 @@
+/* Main Page JS */
+const hamburger = document.querySelector('.hamburger-icon');
+const navLinks = document.querySelector('.nav-links');
+const navLinksArray = document.querySelectorAll('.nav-link');
+const heroSection = document.querySelector('#hero');
+let isVisible = true;
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('open');
+    navLinks.classList.toggle('hidden');
+
+    isVisible = !isVisible;
+    console.log(isVisible);
+
+    if (isVisible) {
+        heroSection.classList.remove('hidden');
+        heroSection.classList.add('visible');
+    } else {
+        heroSection.classList.remove('visible');
+        heroSection.classList.add('hidden');
+    }
+});
+
+// Add event listeners to each nav link
+navLinksArray.forEach((navLink) => {
+    navLink.addEventListener('click', () => {
+        if(window.innerWidth <= 1200){
+            console.log('Hehehehehhehe');
+            hamburger.classList.toggle('open');
+            navLinks.classList.toggle('hidden');
+            heroSection.classList.add('visible');
+            isVisible = true;
+    }
+    });
+});
+
+
+// function logScreenResolution() {
+//     const screenWidth = window.screen.width;
+//     const screenHeight = window.screen.height;
+//     console.log(`Screen Resolution: ${screenWidth}x${screenHeight}`);
+//   }
+  
+//   // Call the function to log the initial screen resolution
+//   logScreenResolution();
+  
+//   // Add an event listener for window resize events to log the new resolution
+//   window.addEventListener('resize', logScreenResolution);
+  
