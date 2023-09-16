c938ceaee3b22f30e386dd85fda127b4e0a3c998
arunnegi-07 <reacharunnegi@gmail.com>
2023-09-16 16:36:11 +0530

    Work in Progress

--- lightDarkMode.js
+++ lightDarkMode.js
@@ -1,31 +1,21 @@
 document.addEventListener("DOMContentLoaded",(e) => {
     const toggleThemeButton = document.getElementById("toggleTheme");
     const themeIcon = document.querySelector('img[alt="theme-icon"]');
-    const githubLogo = document.querySelector('img[alt="github-icon"]');
-    const linkedinLogo = document.querySelector('img[alt="linkedin-icon"]');
 
     const lightLogos = {
-        github: "assets/github_light.png",
-        linkedin: "assets/linkedin_light.png",
-        theme: "assets/theme_light.png",
+        theme: "./assets/theme_light.png",
     }
 
     const darkLogos = {
-        github: "assets/github_dark.png",
-        linkedin: "assets/linkedin_dark.png",
-        theme: "assets/theme_dark.png",
+        theme: "./assets/theme_dark.png",
     }
 
     function setTheme(isDark){
-        githubLogo.src = isDark ? darkLogos.github : lightLogos.github;
-        linkedinLogo.src = isDark ? darkLogos.linkedin : lightLogos.linkedin;
         themeIcon.src = isDark ? darkLogos.theme : lightLogos.theme;
     }
 
     toggleThemeButton.addEventListener('click',() => {
         const isDark = document.documentElement.classList.toggle('dark-theme');
-        console.log(isDark);
-
 
         localStorage.setItem("isDark",isDark);
 
