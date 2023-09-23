7af3d8beeb0b964d050658c70bdbf4ce7dca4a86
arunnegi-07 <reacharunnegi@gmail.com>
2023-09-23 23:09:16 +0530

    Work in Progress

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 /* Main Page JS */
-const hamburger = document.querySelector('.hamburger-icon');
+let hamburger = document.querySelector('.hamburger');
+let spanElements = document.querySelectorAll('.hamburger span');
 const navLinks = document.querySelector('.nav-links');
 const navLinksArray = document.querySelectorAll('.nav-link');
 const heroSection = document.querySelector('#hero');
 let isVisible = true;
- 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('open');
+
+hamburger.addEventListener('click',() =>{
+	hamburger.classList.toggle('active')
     navLinks.classList.toggle('hidden');
 
     isVisible = !isVisible;
@@ -19,17 +20,26 @@ hamburger.addEventListener('click', () => {
         heroSection.classList.remove('visible');
         heroSection.classList.add('hidden');
     }
-});
+	spanElements.forEach(span => {
+		span.classList.remove('no-animation');
+	});
+
+    // Wait for 0.7 seconds and then add the 'no-animation' class back
+    setTimeout(() => {
+        spanElements.forEach(span => {
+            span.classList.add('no-animation');
+        });
+    },700); 
+})
 
 // Add event listeners to each nav link
 navLinksArray.forEach((navLink) => {
     navLink.addEventListener('click', () => {
         if(window.innerWidth <= 1200){
-            console.log('Hehehehehhehe');
-            hamburger.classList.toggle('open');
+            hamburger.classList.toggle('active')
             navLinks.classList.toggle('hidden');
             heroSection.classList.add('visible');
             isVisible = true;
-    }
+        }
     });
 });
